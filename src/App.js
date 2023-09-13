723ed639c99e657198b31c2188cdaf370d9996f0
HafizHammad123 <127588251+Hammad-12345@users.noreply.github.com>
2023-09-13 12:38:15 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import TimeTable from './Pages/TimeTable';
-import Contact from './Pages/Contact';
-import Blog from './Pages/Blog';
-import About from './Pages/About';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import TimeTable from "./Pages/TimeTable";
+import Contact from "./Pages/Contact";
+import Blog from "./Pages/Blog";
+import About from "./Pages/About";
 function App() {
-  return <>
-  <BrowserRouter>
-  <Routes>
-    <Route path={'/'} element={<Home/>}/>
-    <Route path={'/TimeTable'} element={<TimeTable/>}/>
-    <Route path={'/About'} element={<About/>}/>
-    <Route path={'/Blog'} element={<Blog/>}/>
-    <Route path={'/Contact'} element={<Contact/>}/>
-  </Routes>
-  </BrowserRouter>
-  </>
+  return (
+    <>
+      <BrowserRouter basename="/Doctor_App_Host">
+        <Routes>
+          <Route exact path={"/"} element={<Home />} />
+          <Route path={"/TimeTable"} element={<TimeTable />} />
+          <Route path={"/About"} element={<About />} />
+          <Route path={"/Blog"} element={<Blog />} />
+          <Route path={"/Contact"} element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,88 +1,118 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 export default function Header() {
-  const [ShowState, UpdateShowState] = useState(true)
+  const [ShowState, UpdateShowState] = useState(true);
   const ShowSearchBar = () => {
     if (!ShowState) {
-      UpdateShowState(true)
+      UpdateShowState(true);
+    } else {
+      UpdateShowState(false);
     }
-    else {
-      UpdateShowState(false)
-    }
-  }
-  useEffect(()=>
-  {
-AOS.init({duration:2000})
-  },[])
-  return <>
-    {
-      ShowState ?
+  };
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+  return (
+    <>
+      {ShowState ? (
         <>
-          <nav className="navbar navbar-expand-lg navbar-light navbar_main pt-3 pb-3" data-aos="fade-up">
+          <nav
+            className="navbar navbar-expand-lg navbar-light navbar_main pt-3 pb-3"
+            data-aos="fade-up"
+          >
             <div className="container-fluid ">
               <div className="d-lg-none">
-                <img src="https://demo.phlox.pro/medical/wp-content/uploads/sites/166/2020/05/footer.jpg" width={"100px"} height={"100px"} alt="" srcset="" />
+                <img
+                  src="https://demo.phlox.pro/medical/wp-content/uploads/sites/166/2020/05/footer.jpg"
+                  width={"100px"}
+                  height={"100px"}
+                  alt=""
+                  srcset=""
+                />
               </div>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
                 <span class="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse  justify-content-end" id="navbarSupportedContent">
+              <div
+                class="collapse navbar-collapse  justify-content-end"
+                id="navbarSupportedContent"
+              >
                 <ul class="navbar-nav mb-2 mb-lg-0 gap-4 ">
                   <li class="nav-item ">
-                    <Link class="nav-link active text-light fw-normal" aria-current="page" to={"/"}>Home</Link>
+                    <Link
+                      class="nav-link active text-light fw-normal"
+                      aria-current="page"
+                      to={"/"}
+                    >
+                      Home
+                    </Link>
                   </li>
                   <li class="nav-item">
-                    <Link class="nav-link text-light fw-normal" to={'/TimeTable'}>Time Table</Link>
+                    <Link
+                      class="nav-link text-light fw-normal"
+                      to={"/TimeTable"}
+                    >
+                      Time Table
+                    </Link>
                   </li>
                   <li class="nav-item ">
-                    <Link class="nav-link text-light fw-normal" to={"/About"} >
+                    <Link class="nav-link text-light fw-normal" to={"/About"}>
                       About
                     </Link>
-
                   </li>
                   <li class="nav-item ">
-                    <Link class="nav-link text-light fw-normal" to={"/Blog"} >
+                    <Link class="nav-link text-light fw-normal" to={"/Blog"}>
                       Blog
                     </Link>
-
                   </li>
                   <li class="nav-item ">
                     <Link class="nav-link text-light fw-normal" to={"/Contact"}>
                       Contact
                     </Link>
-
                   </li>
                   <li class="nav-item ">
-                    <button className="nav-link border-0 btn text-light" onClick={ShowSearchBar}>
+                    <button
+                      className="nav-link border-0 btn text-light"
+                      onClick={ShowSearchBar}
+                    >
                       <i class="fa-solid fa-magnifying-glass"></i>
                     </button>
-
                   </li>
-
-
-
                 </ul>
-
               </div>
             </div>
           </nav>
         </>
-        :
+      ) : (
         <>
           <div className="container mt-2">
             <div className="row justify-content-center">
               <div className="col-10 position-relative">
-                <input type="text" className="form-control pt-3 pb-3 border-top-0 border-start-0 border-end-0 border-bottom-1 rounded-0 border-dark" />
-                <button className="btn position-absolute top-0 end-0 me-2 fs-4" onClick={ShowSearchBar}><i class="fa-solid fa-xmark"></i></button>
+                <input
+                  type="text"
+                  className="form-control pt-3 pb-3 border-top-0 border-start-0 border-end-0 border-bottom-1 rounded-0 border-dark"
+                />
+                <button
+                  className="btn position-absolute top-0 end-0 me-2 fs-4"
+                  onClick={ShowSearchBar}
+                >
+                  <i class="fa-solid fa-xmark"></i>
+                </button>
               </div>
             </div>
-
           </div>
         </>
-    }
-
-
-  </>
+      )}
+    </>
+  );
 }
